2e3a2b6e3d5786ecd553b2de2594c291ad60086f
qianduoduolr <656146095@qq.com>
2021-10-26 21:26:52 +0800

    .

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -8,4 +8,3 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test_prop.py --config $CONFIG --checkpoint $CHECKPOINT  --launcher pytorch ${@:4}
-
