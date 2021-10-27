d95c97b5801a7ca47869f7a3f26b22b7d6d89ad5
qianduoduolr <656146095@qq.com>
2021-10-27 13:14:57 +0800

    auto pbs

--- configs/train/vqvae_mlm_d4_nemd2048_ps11_t2_nofc_orivq.py
+++ configs/train/vqvae_mlm_d4_nemd2048_ps11_t2_nofc_orivq.py
@@ -1,6 +1,7 @@
 import os
-exp_name = os.path.basename(__file__)[:-3]
-docker_name='bit:5000/lirui_torch1.5_cuda10.1_corr'
+# exp_name = os.path.basename(os.path.abs)[:-3]
+exp_name = 'vqvae_mlm_d4_nemd2048_ps11_t2_nofc_orivq'
+docker_name = 'bit:5000/lirui_torch1.5_cuda10.1_corr'
 
 # model settings
 model = dict(
