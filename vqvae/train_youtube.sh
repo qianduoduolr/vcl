0b947f4014a89fa3c0d9dcd654389800ba5bdd3d
qianduoduolr <656146095@qq.com>
2021-10-29 15:13:43 +0800

    vqvae youtube train

--- vqvae/train_youtube.sh
+++ vqvae/train_youtube.sh
@@ -7,11 +7,3 @@ python /gdata/lirui/project/vcl/vqvae/train_vqvae.py \
 python /gdata/lirui/project/vcl/vqvae/train_vqvae.py \
 --downsample 2 --n_embed 4096 --train_epoch 400 --dataset youtube --data_path /gdata/lirui/dataset/YouTube-VOS \
 --list_path /gdata/lirui/dataset/YouTube-VOS/2018/train --in_c 256 --res_c 128 --emb_c 128 --save_path /gdata/lirui/models/vqvae
-
-python /gdata/lirui/project/vcl/vqvae/train_vqvae.py \
---downsample 2 --n_embed 4096 --train_epoch 3 --dataset imagenet --data_path /gdata/lirui/dataset/YouTube-VOS \
---list_path /gdata/lirui/dataset/YouTube-VOS/2018/train --in_c 256 --res_c 128 --emb_c 128 --save_path /gdata/lirui/models/vqvae
-
-python /gdata/lirui/project/vcl/vqvae/train_vqvae.py \
---downsample 2 --n_embed 4096 --train_epoch 3 --dataset imagenet --data_path /gdata/lirui/dataset/YouTube-VOS \
---list_path /gdata/lirui/dataset/YouTube-VOS/2018/train --in_c 256 --res_c 128 --emb_c 128 --save_path /gdata/lirui/models/vqvae
