2649cddf688b9aa7eea99067606f6b14977eb7c9
qianduoduolr <656146095@qq.com>
2021-10-31 16:10:34 +0800

    .

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -6,4 +6,7 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 2 --launcher pytorch ${@:4}
+
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 3 --launcher pytorch ${@:4}
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -5,8 +5,8 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-#     $(dirname "$0")/train.py --config $CONFIG --launcher pytorch ${@:3}
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py --config $CONFIG --launcher pytorch ${@:3}
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test_prop.py --config $CONFIG --out-indices 2 --launcher pytorch ${@:3}
