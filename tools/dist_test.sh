c5157275dd811054a070cebbe20a08b02bef80e3
qianduoduolr <656146095@qq.com>
2021-10-31 16:25:26 +0800

    .

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -6,7 +6,7 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 2 --launcher pytorch ${@:4}
+    $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 2 --launcher pytorch ${@:3}
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 3 --launcher pytorch ${@:4}
+    $(dirname "$0")/test_prop.py --config $CONFIG  --out-indices 3 --launcher pytorch ${@:3}
