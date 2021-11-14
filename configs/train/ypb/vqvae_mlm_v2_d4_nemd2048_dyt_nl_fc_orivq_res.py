4360ea190a639d70eddca3e40af977eeb5a2fb0a
qianduoduolr <656146095@qq.com>
2021-11-14 21:30:58 +0800

    .

--- configs/train/ypb/vqvae_mlm_v2_d4_nemd2048_dyt_nl_fc_orivq_res.py
+++ configs/train/ypb/vqvae_mlm_v2_d4_nemd2048_dyt_nl_fc_orivq_res.py
@@ -147,10 +147,10 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = f'/gdata/lirui/expdir/VCL/group_vqvae_tracker/{exp_name}'
 
-# eval_config= dict(
-#                   output_dir=f'{work_dir}/eval_output',
-#                   checkpoint_path=f'/gdata/lirui/expdir/VCL/group_vqvae_tracker/{exp_name}/epoch_{max_epoch}.pth'
-#                 )
+eval_config= dict(
+                  output_dir=f'{work_dir}/eval_output',
+                  checkpoint_path=f'/gdata/lirui/expdir/VCL/group_vqvae_tracker/{exp_name}/epoch_{max_epoch}.pth'
+                )
 
 
 load_from = None
---
+++ tools/dist_train_v2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+GPUS=$2
+PORT=${PORT:-29500}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py --config $CONFIG --launcher pytorch ${@:3}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/test_prop.py --config $CONFIG --out-indices 0 --launcher pytorch ${@:3}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/test_prop.py --config $CONFIG --out-indices 1 --launcher pytorch ${@:3}
