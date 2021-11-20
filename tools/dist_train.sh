75c6364d348a4a330c1fcfe1ca36ea9f145ffc65
qianduoduolr <656146095@qq.com>
2021-11-20 15:17:34 +0800

    .

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -5,9 +5,9 @@ mkdir -p /dev/shm/2018/train
 mkdir -p  /dev/shm/2018/train_all_frames
 # cd 2018/train
 cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train/zip_data/JPEGImages_s256.zip /dev/shm/2018/train/
-unzip -d /dev/shm/2018/train/ /dev/shm/2018/train/zip_data/JPEGImages_s256.zip
+unzip -d /dev/shm/2018/train/ /dev/shm/2018/train/JPEGImages_s256.zip
 cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train_all_frames/zip_data/Flows_s256.zip /dev/shm/2018/train_all_frames/
-unzip -d /dev/shm/2018/train_all_frames/ /dev/shm/2018/train_all_frames/zip_data/Flows_s256.zip
+unzip -d /dev/shm/2018/train_all_frames/ /dev/shm/2018/train_all_frames/Flows_s256.zip
 cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train/*.txt /dev/shm/2018/train/
 echo "finish cp data"
 
