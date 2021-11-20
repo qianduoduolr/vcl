6d4f546ace427f3a5b60ef81e4faa146c4742c52
qianduoduolr <656146095@qq.com>
2021-11-20 20:08:58 +0800

    .

--- configs/train/local/vqvae_mlm_d4_nemd2048_dyt_nl_l2_nofc_orivq_area0.2.py
+++ configs/train/local/vqvae_mlm_d4_nemd2048_dyt_nl_l2_nofc_orivq_area0.2.py
@@ -66,7 +66,7 @@ val_pipeline = [
 
 # demo_pipeline = None
 data = dict(
-    workers_per_gpu=4,
+    workers_per_gpu=2,
     train_dataloader=dict(samples_per_gpu=5, drop_last=True),  # 4 gpus
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1, workers_per_gpu=1),
--- configs/train/ypb/dist_nl_l2_orivq_motion.py
+++ configs/train/ypb/dist_nl_l2_orivq_motion.py
@@ -77,7 +77,7 @@ val_pipeline = [
 
 # demo_pipeline = None
 data = dict(
-    workers_per_gpu=4,
+    workers_per_gpu=2,
     train_dataloader=dict(samples_per_gpu=16, drop_last=True),  # 4 gpus
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1, workers_per_gpu=1),
