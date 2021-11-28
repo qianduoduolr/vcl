024f34d54fae84812d1debce07388324afb64ffb
qianduoduolr <656146095@qq.com>
2021-11-28 19:15:34 +0800

    .

--- tools/dist_train_dry_run.sh
+++ tools/dist_train_dry_run.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
+mkdir -p /dev/shm/2018/train
+mkdir -p  /dev/shm/2018/train_all_frames
+# cd 2018/train
+cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train/zip_data/JPEGImages_s256.zip /dev/shm/2018/train/
+unzip -d /dev/shm/2018/train/ /dev/shm/2018/train/JPEGImages_s256.zip
+cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train_all_frames/zip_data/Flows_s256.zip /dev/shm/2018/train_all_frames/
+unzip -d /dev/shm/2018/train_all_frames/ /dev/shm/2018/train_all_frames/Flows_s256.zip
+cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train/*.txt /dev/shm/2018/train/
+cp -r /gdata/lirui/dataset/YouTube-VOS/2018/train/*.json /dev/shm/2018/train/
+echo "finish cp data"
+
 CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
 
-for variable in {1..1000000000}
+for variable in {1..100000}
 do   
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train_dry_run.py --config $CONFIG --launcher pytorch ${@:3}
+    echo 'start training'
+    PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+        $(dirname "$0")/train_dry_run.py --config $CONFIG --launcher pytorch ${@:3}
 
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_prop_dry_run.py --config $CONFIG --out-indices 2 --launcher pytorch ${@:3}
+    PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+        $(dirname "$0")/test_prop_dry_run.py --config $CONFIG --out-indices 2 --launcher pytorch ${@:3}
 
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_prop_dry_run.py --config $CONFIG --out-indices 3 --launcher pytorch ${@:3}
-    
-echo "$variable finished !"
+    PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+        $(dirname "$0")/test_prop_dry_run.py --config $CONFIG --out-indices 3 --launcher pytorch ${@:3}
+        
+    echo "$variable finished !"
 done  
--- tools/train_dry_run.py
+++ tools/train_dry_run.py
@@ -19,7 +19,7 @@ from vcl.utils import collect_env, get_root_logger
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train an editor')
-    parser.add_argument('--config', help='train config file path', default='/home/lr/project/vcl/configs/train/local/vqvae_mlm_d4_nemd8_dyt_nl_l2_fc_orivq_motion.py')
+    parser.add_argument('--config', help='train config file path', default='/home/lr/project/vcl/configs/train/local/train_vqvae_video_d4_nemd32_contrastive_byol_commit1.0.py')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
@@ -57,6 +57,97 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    cfg = Config.fromfile(args.config)
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+    # update configs according to CLI args
+    if args.work_dir is not None:
+        cfg.work_dir = args.work_dir
+    if args.resume_from is not None:
+        cfg.resume_from = args.resume_from
+    cfg.gpus = args.gpus
+
+    if args.autoscale_lr:
+        # apply the linear scaling rule (https://arxiv.org/abs/1706.02677)
+        cfg.optimizer['lr'] = cfg.optimizer['lr'] * cfg.gpus / 8
+
+    # init distributed env first, since logger depends on the dist info.
+    if args.launcher == 'none':
+        distributed = False
+    else:
+        distributed = True
+        init_dist(args.launcher, **cfg.dist_params)
+
+    # create work_dir
+    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+
+    # cp code to work_dir
+    if distributed:
+        file_path = osp.dirname(osp.dirname(osp.abspath(__file__)))
+        # os.system(f"rsync -a --exclude='{file_path}/.git' {file_path} {cfg.work_dir}")
+        os.system(f"cp -r {file_path} {cfg.work_dir}")
+
+    # init the logger before other steps
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
+    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
+
+    # log env info
+    env_info_dict = collect_env.collect_env()
+    env_info = '\n'.join([f'{k}: {v}' for k, v in env_info_dict.items()])
+    dash_line = '-' * 60 + '\n'
+    logger.info('Environment info:\n' + dash_line + env_info + '\n' +
+                dash_line)
+
+    # log some basic info
+    logger.info('Distributed training: {}'.format(distributed))
+    logger.info('Config:\n{}'.format(cfg.text))
+
+    # set random seeds
+    if args.seed is not None:
+        logger.info('Set random seed to {}, deterministic: {}'.format(
+            args.seed, args.deterministic))
+        set_random_seed(args.seed, deterministic=args.deterministic)
+    cfg.seed = args.seed
+
+    model = build_model(
+        cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
+    
+    if cfg.train_cfg.get('syncbn', False) and distributed:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+
+    datasets = [build_dataset(cfg.data.train)]
+
+    if len(cfg.workflow) == 2:
+        val_dataset = copy.deepcopy(cfg.data.val)
+        val_dataset.pipeline = cfg.data.train.pipeline
+        datasets.append(build_dataset(val_dataset))
+    if cfg.checkpoint_config is not None:
+        # save version, config file content and class names in
+        # checkpoints as meta data
+        cfg.checkpoint_config.meta = dict(
+            config=cfg.text,
+        )
+
+    # meta information
+    meta = dict()
+    if cfg.get('exp_name', None) is None:
+        cfg['exp_name'] = osp.splitext(osp.basename(cfg.work_dir))[0]
+    meta['exp_name'] = cfg.exp_name
+    meta['seed'] = args.seed
+    meta['env_info'] = env_info
+
+    # add an attribute for visualization convenience
+    train_model(
+        model,
+        datasets,
+        cfg,
+        distributed=distributed,
+        validate=(not args.no_validate),
+        timestamp=timestamp,
+        meta=meta)
 
 
 if __name__ == '__main__':
