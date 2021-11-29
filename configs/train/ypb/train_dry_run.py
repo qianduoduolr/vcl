e00bfcd379a27a0702ff0e017898a55b74df2f55
qianduoduolr <656146095@qq.com>
2021-11-29 19:11:44 +0800

    .

--- configs/train/ypb/train_dry_run.py
+++ configs/train/ypb/train_dry_run.py
@@ -3,19 +3,23 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from vcl.utils import *
 
-exp_name = 'vqvae_mlm_d4_nemd32_dyt_nl_l2_fc_orivq_motionwithbbox'
+exp_name = 'vqvae_mlm_d4_nemd32_byol_dyt_nl_l2_fc_orivq_withbbox_notsame'
 docker_name = 'bit:5000/lirui_torch1.8_cuda11.1_corr'
 
 # model settings
 model = dict(
     type='Vqvae_Tracker',
     backbone=dict(type='ResNet',depth=18, strides=(1, 2, 1, 1), out_indices=(3, )),
-    vqvae=dict(type='VQVAE', downsample=4, n_embed=32, channel=256, n_res_channel=128, embed_dim=128),
+    vqvae=dict(type='VQCL_v2', backbone=dict(type='ResNet', depth=18, strides=(1, 2, 1, 1), out_indices=(3, )),
+               sim_siam_head=dict(type='SimSiamHead', in_channels=512, num_projection_fcs=3, projection_mid_channels=512,
+               projection_out_channels=512, num_predictor_fcs=2, predictor_mid_channels=128, predictor_out_channels=512,
+               with_norm=True, spatial_type='avg'),loss=dict(type='CosineSimLoss', negative=False), embed_dim=128,
+               n_embed=32, commitment_cost=1.0,),
     ce_loss=dict(type='Ce_Loss',reduction='none'),
     patch_size=-1,
     fc=True,
     temperature=0.1,
-    pretrained_vq='/gdata/lirui/models/vqvae/vqvae_youtube_d4_n32_c256_embc128.pth',
+    pretrained_vq='/gdata/lirui/models/vqvae/vqvae_youtube_d4_n32_c256_embc128_byol_commit1.0.pth',
     pretrained=None
 )
 
@@ -47,9 +51,11 @@ img_norm_cfg = dict(
 #     mean=[0, 0, 0], std=[255, 255, 255], to_bgr=False)
 
 train_pipeline = [
-    dict(type='RandomResizedCrop', area_range=(0.6,1.0), aspect_ratio_range=(1.5, 2.0),),
+    dict(type='RandomResizedCrop', area_range=(0.6,1.0), aspect_ratio_range=(1.5, 2.0),same_across_clip=False,
+        same_on_clip=False),
     dict(type='Resize', scale=(256, 256), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Flip', flip_ratio=0.5, same_across_clip=False,
+        same_on_clip=False),
     dict(
         type='ColorJitter',
         brightness=0.7,
@@ -97,8 +103,8 @@ data = dict(
             list_path='/dev/shm/2018/train',
             data_prefix=dict(RGB='train/JPEGImages_s256', FLOW='train_all_frames/Flows_s256', ANNO='train/Annotations'),
             mask_ratio=0.15,
-            clip_length=2,
-            num_clips=1,
+            clip_length=1,
+            num_clips=2,
             vq_size=32,
             pipeline=train_pipeline,
             test_mode=False),
@@ -131,7 +137,7 @@ lr_config = dict(
     warmup_by_epoch=True
     )
 
-checkpoint_config = dict(interval=200, save_optimizer=True, by_epoch=True)
+checkpoint_config = dict(interval=400, save_optimizer=True, by_epoch=True)
 # remove gpu_collect=True in non distributed training
 # evaluation = dict(interval=1000, save_image=False, gpu_collect=False)
 log_config = dict(
