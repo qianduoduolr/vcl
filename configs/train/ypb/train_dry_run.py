1ccb00b57e58593514c0ce38d2e00159f2028d67
qianduoduolr <656146095@qq.com>
2021-12-09 13:42:53 +0800

    .

--- configs/train/ypb/train_dry_run.py
+++ configs/train/ypb/train_dry_run.py
@@ -3,26 +3,17 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from vcl.utils import *
 
-exp_name = 'vqvae_mlm_d4_nemd32-32_byol_dyt_nl_l2_fc_orivq_withbbox_random_0.5'
-docker_name = 'bit:5000/lirui_torch1.8_cuda11.1_corr'
+exp_name = 'vqvae_d4_nemd2048_withbbox_mse'
+docker_name = 'bit:5000/lirui_torch1.8_cuda11.1_corres'
 
 # model settings
 model = dict(
-    type='Vqvae_Tracker',
+    type='Vqvae_Tracker_V3',
     backbone=dict(type='ResNet',depth=18, strides=(1, 2, 1, 1), out_indices=(3, )),
-    vqvae=[dict(type='VQCL_v2', backbone=dict(type='ResNet', depth=18, strides=(1, 2, 1, 1), out_indices=(3, )),
-               sim_siam_head=dict(type='SimSiamHead', in_channels=512, num_projection_fcs=3, projection_mid_channels=512,
-               projection_out_channels=512, num_predictor_fcs=2, predictor_mid_channels=128, predictor_out_channels=512,
-               with_norm=True, spatial_type='avg'),loss=dict(type='CosineSimLoss', negative=False), embed_dim=128,
-               n_embed=32, commitment_cost=1.0,),
-            dict(type='VQVAE', downsample=4, n_embed=32, channel=256, n_res_channel=128, embed_dim=128)],
-    ce_loss=dict(type='Ce_Loss',reduction='none'),
-    multi_head_weight=[1.0,1.0],
+    vqvae=dict(type='VQVAE', downsample=4, n_embed=2048, channel=256, n_res_channel=128, embed_dim=128),
+    pretrained_vq='/gdata/lirui/models/vqvae/vqvae_youtube_d4_n2048_c256_embc128.pth',
     patch_size=-1,
-    fc=True,
-    temperature=0.1,
-    pretrained_vq=['/gdata/lirui/models/vqvae/vqvae_youtube_d4_n32_c256_embc128_byol_commit1.0.pth',
-                    '/gdata/lirui/models/vqvae/vqvae_youtube_d4_n32_c256_embc128_withbbox.pth'],
+    mse_loss=dict(type='MSELoss',reduction='mean', loss_weight=1),
     pretrained=None
 )
 
@@ -41,7 +32,7 @@ test_cfg = dict(
     output_dir='eval_results')
 
 # dataset settings
-train_dataset_type = 'VOS_youtube_dataset_mlm_withbbox_random'
+train_dataset_type = 'VOS_youtube_dataset_rgb_withbbox'
 
 val_dataset_type = None
 test_dataset_type = 'VOS_davis_dataset_test'
@@ -50,13 +41,13 @@ test_dataset_type = 'VOS_davis_dataset_test'
 # train_pipeline = None
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
-# img_norm_cfg = dict(
-#     mean=[0, 0, 0], std=[255, 255, 255], to_bgr=False)
 
 train_pipeline = [
-    dict(type='RandomResizedCrop', area_range=(0.6,1.0), aspect_ratio_range=(1.5, 2.0),),
+    dict(type='RandomResizedCrop', area_range=(0.6,1.0), aspect_ratio_range=(1.5, 2.0),same_across_clip=False,
+        same_on_clip=False),
     dict(type='Resize', scale=(256, 256), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
+    dict(type='Flip', flip_ratio=0.5, same_across_clip=False,
+        same_on_clip=False),
     dict(
         type='ColorJitter',
         brightness=0.7,
@@ -71,8 +62,8 @@ train_pipeline = [
     dict(type='Normalize', **img_norm_cfg, keys='jitter_imgs'),
     dict(type='FormatShape', input_format='NPTCHW'),
     dict(type='FormatShape', input_format='NPTCHW', keys='jitter_imgs'),
-    dict(type='Collect', keys=['imgs', 'jitter_imgs', 'mask_query_idx'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'jitter_imgs', 'mask_query_idx'])
+    dict(type='Collect', keys=['imgs', 'jitter_imgs'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'jitter_imgs'])
 ]
 
 val_pipeline = [
@@ -98,16 +89,13 @@ data = dict(
     train=
             dict(
             type=train_dataset_type,
-            size=256,
-            p=0.5,
             root='/dev/shm',
             list_path='/dev/shm/2018/train',
-            data_prefix=dict(RGB='train/JPEGImages_s256', FLOW='train_all_frames/Flows_s256', ANNO='train/Annotations'),
-            mask_ratio=0.15,
-            clip_length=2,
-            vq_size=32,
-            pipeline=train_pipeline,
-            test_mode=False),
+            data_prefix=dict(RGB='train/JPEGImages_s256', ANNO='train/Annotations'),
+            clip_length=1,
+            num_clips=2,
+            pipeline=train_pipeline
+            ),
 
     test =  dict(
             type=test_dataset_type,
@@ -122,13 +110,11 @@ data = dict(
 # optimizer
 optimizers = dict(
     backbone=dict(type='Adam', lr=0.001, betas=(0.9, 0.999)),
-    predictor=dict(type='Adam', lr=0.001, betas=(0.9, 0.999)),
-    predictor1=dict(type='Adam', lr=0.001, betas=(0.9, 0.999))
     )
 # learning policy
 # total_iters = 200000
 runner_type='epoch'
-max_epoch=3200
+max_epoch=800
 lr_config = dict(
     policy='CosineAnnealing',
     min_lr_ratio=0.001,
@@ -138,7 +124,7 @@ lr_config = dict(
     warmup_by_epoch=True
     )
 
-checkpoint_config = dict(interval=1600, save_optimizer=True, by_epoch=True)
+checkpoint_config = dict(interval=400, save_optimizer=True, by_epoch=True)
 # remove gpu_collect=True in non distributed training
 # evaluation = dict(interval=1000, save_image=False, gpu_collect=False)
 log_config = dict(
