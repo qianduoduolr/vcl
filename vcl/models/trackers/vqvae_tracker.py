c9788ac0ff6e5db575abb003b651587beaa2838f
qianduoduolr <656146095@qq.com>
2021-12-11 22:18:37 +0800

    .

--- vcl/models/trackers/vqvae_tracker.py
+++ vcl/models/trackers/vqvae_tracker.py
@@ -624,3 +624,191 @@ class Vqvae_Tracker_V2(BaseModel):
 
         return out, att
 
+@MODELS.register_module()
+class Vqvae_Tracker_V4(BaseModel):
+
+    def __init__(self,
+                 backbone,
+                 vqvae,
+                 patch_size,
+                 pretrained_vq,
+                 temperature,
+                 sim_siam_head=None,
+                 multi_head_weight=[1.0],
+                 ce_loss=None,
+                 mse_loss=None,
+                 fc=True,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None
+                 ):
+        """ Space-Time Memory Network for Video Object Segmentation
+
+        Args:
+            depth ([type]): ResNet depth for encoder
+            pixel_loss ([type]): loss option
+            train_cfg ([type], optional): [description]. Defaults to None.
+            test_cfg ([type], optional): [description]. Defaults to None.
+            pretrained ([type], optional): [description]. Defaults to None.
+        """
+        super(Vqvae_Tracker_V4, self).__init__()
+        if not isinstance(vqvae, list):
+            vqvae = list([vqvae])
+            pretrained_vq = list([pretrained_vq])
+
+        self.train_cfg = train_cfg
+        self.test_cfg = test_cfg
+        self.patch_size = patch_size
+        self.num_head = len(vqvae)
+        self.multi_head_weight = multi_head_weight
+
+        logger = get_root_logger()
+
+        self.backbone = build_backbone(backbone)
+        if sim_siam_head is not None:
+            self.head = build_components(sim_siam_head)
+            self.head.init_weights()
+        else:
+            self.head = None
+
+        self.vq_type = vqvae[0].type
+        if self.vq_type != 'DALLE_Encoder':
+            for idx, i in enumerate(range(len(vqvae))):
+                i = str(i).replace('0', '')
+                setattr(self, f'vqvae{i}', build_model(vqvae[idx]).cuda())
+                _ = load_checkpoint(getattr(self, f'vqvae{i}'), pretrained_vq[idx], map_location='cpu')
+                logger.info(f'load {i}th pretrained VQVAE successfully')
+                setattr(self, f'vq_emb{i}', getattr(self, f'vqvae{i}').quantize.embed)
+                setattr(self, f'n_embed{i}', vqvae[idx].n_embed)
+                setattr(self, f'vq_t{i}', temperature)
+                setattr(self, f'vq_enc{i}', getattr(self, f'vqvae{i}').encode)
+                setattr(self, f'vq_dec{i}', getattr(self, f'vqvae{i}').decode) if idx >= 1 else None
+
+        else:
+            assert self.num_head == 1
+            self.vq_enc = load_model(pretrained_vq).cuda()
+            self.n_embed = self.vq_enc.vocab_size
+            logger.info('load pretrained VQVAE successfully')
+
+        # loss
+        self.ce_loss = build_loss(ce_loss) if ce_loss else None
+        self.mse_loss = build_loss(mse_loss) if mse_loss else None
+
+        # corr
+        if patch_size != -1:
+            from spatial_correlation_sampler import SpatialCorrelationSampler
+            self.correlation_sampler = SpatialCorrelationSampler(
+                kernel_size=1,
+                patch_size=patch_size,
+                stride=1,
+                padding=0,
+                dilation=1)
+
+        # fc
+        self.fc = fc
+        for i in range(len(vqvae)):
+            if i > 1: break
+            i = str(i).replace('0', '')
+            setattr(self, f'predictor{i}', nn.Linear(self.backbone.feat_dim, getattr(self, f'n_embed{i}')))
+    
+
+        if pretrained:
+            _ = load_checkpoint(self, pretrained, map_location='cpu')
+            logger.info('load pretrained model successfully')
+
+    def forward_train(self, imgs, mask_query_idx, jitter_imgs=None):
+
+        bsz, num_clips, t, c, h, w = imgs.shape
+        
+        embeds = []
+        out_ind = []
+        # vqvae tokenize for query frame
+        with torch.no_grad():
+            for idx, i in enumerate(range(self.num_head)):
+                i = str(i).replace('0', '')
+                vqvae = getattr(self, f'vqvae{i}')
+                vq_enc = getattr(self, f'vq_enc{i}')
+                vqvae.eval()
+                if idx >= 1:
+                    for i in range(t):
+                        embed, quant, _, ind, _ = vq_enc(imgs[:, 0, i])
+                        embeds.append(embed.flatten(1,2))
+                else:
+                    _, _, _, ind, _ = vq_enc(imgs[:, 0, -1])
+                    ind = ind.unsqueeze(1).repeat(1, t-1, 1, 1).reshape(-1, 1).long().detach()
+                out_ind.append(ind)
+        embeds_refs = torch.stack(embeds[:-1], 1)
+
+
+        mask_query_idx = mask_query_idx.bool().unsqueeze(1).repeat(1,t-1,1)
+
+        tar = self.backbone(jitter_imgs[:,0,-1])
+        refs = list([self.backbone(jitter_imgs[:,0,i]) for i in range(t-1)])
+
+        if self.patch_size != -1:
+            out, att = self.local_attention(tar, refs, bsz, t)
+        else:
+            out, att = self.non_local_attention_split(tar, refs, bsz)
+
+        losses = {}
+
+        if self.ce_loss:
+            predict = self.predictor(out.flatten(0,2))
+            loss = self.ce_loss(predict, out_ind[0])
+            losses['ce_loss'] = (loss * mask_query_idx.reshape(-1)).sum() / mask_query_idx.sum()
+
+        if self.mse_loss:
+            trans_embeds = torch.einsum('btij,btjc->btic', [att, embeds_refs])
+            trans_embeds = trans_embeds.flatten(0,1).reshape(-1, *embed.shape[1:])
+            trans_quants = self.vq_enc1(trans_embeds, encoding=False)[1]
+            decs = self.vq_dec1(trans_quants)
+            target = imgs[:,0,-1:].repeat(1,t-1,1,1,1).flatten(0,1)
+            losses['mse_loss'] = self.mse_loss(decs, target)
+
+        return losses
+
+
+    def forward(self, test_mode=False, **kwargs):
+
+        if test_mode:
+            return self.forward_test(**kwargs)
+
+        return self.forward_train(**kwargs)
+
+    def train_step(self, data_batch, optimizer, progress_ratio):
+
+        # parser loss
+        losses = self(**data_batch, test_mode=False)
+        loss, log_vars = self.parse_losses(losses)
+
+        # optimizer
+        for k,opz in optimizer.items():
+            opz.zero_grad()
+
+        loss.backward()
+        for k,opz in optimizer.items():
+            opz.step()
+
+        log_vars.pop('loss')
+        outputs = dict(
+            log_vars=log_vars,
+            num_samples=len(data_batch['imgs'])
+        )
+
+        return outputs
+
+    
+    def non_local_attention_split(self, tar, refs, bsz):
+
+        refs = torch.stack(refs, 1)
+        _, t, feat_dim, w_, h_ = refs.shape
+
+        tar = tar.flatten(2).permute(0, 2, 1)
+        refs = refs.flatten(3).permute(0, 1, 3, 2)
+        att = torch.einsum("bic,btjc -> btij", (tar, refs))
+        att = F.softmax(att, dim=-1)
+
+        out = torch.matmul(att, refs)
+
+        return out, att
+    
