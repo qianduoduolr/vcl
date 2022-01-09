afcc7a7a6dfddf69a8ffe55f3f84dd42ecd2e044
qianduoduolr <656146095@qq.com>
2022-01-09 21:24:26 +0800

    .

--- configs/train/ypb/train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.25_v2_2.py
+++ configs/train/ypb/train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.25_v2_2.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from vcl.utils import *
 
-exp_name = 'train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.25_v2'
+exp_name = 'train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.25_v2_2'
 docker_name = 'bit:5000/lirui_torch1.5_cuda10.1_corres'
 
 # model settings
