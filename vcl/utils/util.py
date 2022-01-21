71db12ebbb3aeefdecb3dd7239857d4776609875
qianduoduolr <656146095@qq.com>
2022-01-22 01:48:09 +0800

    .

--- vcl/utils/util.py
+++ vcl/utils/util.py
@@ -306,3 +306,87 @@ def images2video(imgs, clip_len):
                                 *imgs.shape[2:]).transpose(1, 2).contiguous()
 
     return new_imgs
+
+
+def gaussian(window_size, sigma):
+    def gauss_fcn(x):
+        return -(x - window_size // 2)**2 / float(2 * sigma**2)
+    gauss = torch.stack(
+        [torch.exp(torch.tensor(gauss_fcn(x))) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def get_gaussian_kernel(ksize: int, sigma: float) -> torch.Tensor:
+    r"""Function that returns Gaussian filter coefficients.
+
+    Args:
+        ksize (int): filter size. It should be odd and positive.
+        sigma (float): gaussian standard deviation.
+
+    Returns:
+        Tensor: 1D tensor with gaussian filter coefficients.
+
+    Shape:
+        - Output: :math:`(ksize,)`
+
+    Examples::
+
+        >>> tgm.image.get_gaussian_kernel(3, 2.5)
+        tensor([0.3243, 0.3513, 0.3243])
+
+        >>> tgm.image.get_gaussian_kernel(5, 1.5)
+        tensor([0.1201, 0.2339, 0.2921, 0.2339, 0.1201])
+    """
+    if not isinstance(ksize, int) or ksize % 2 == 0 or ksize <= 0:
+        raise TypeError("ksize must be an odd positive integer. Got {}"
+                        .format(ksize))
+    window_1d: torch.Tensor = gaussian(ksize, sigma)
+    return window_1d
+
+
+
+def get_gaussian_kernel2d(ksize, sigma) -> torch.Tensor:
+    r"""Function that returns Gaussian filter matrix coefficients.
+
+    Args:
+        ksize ([int, int]): filter sizes in the x and y direction.
+         Sizes should be odd and positive.
+        sigma ([int, int]): gaussian standard deviation in the x and y
+         direction.
+
+    Returns:
+        Tensor: 2D tensor with gaussian filter matrix coefficients.
+
+    Shape:
+        - Output: :math:`(ksize_x, ksize_y)`
+
+    Examples::
+
+        >>> tgm.image.get_gaussian_kernel2d((3, 3), (1.5, 1.5))
+        tensor([[0.0947, 0.1183, 0.0947],
+                [0.1183, 0.1478, 0.1183],
+                [0.0947, 0.1183, 0.0947]])
+
+        >>> tgm.image.get_gaussian_kernel2d((3, 5), (1.5, 1.5))
+        tensor([[0.0370, 0.0720, 0.0899, 0.0720, 0.0370],
+                [0.0462, 0.0899, 0.1123, 0.0899, 0.0462],
+                [0.0370, 0.0720, 0.0899, 0.0720, 0.0370]])
+    """
+    if not isinstance(ksize, tuple) or len(ksize) != 2:
+        raise TypeError("ksize must be a tuple of length two. Got {}"
+                        .format(ksize))
+    if not isinstance(sigma, tuple) or len(sigma) != 2:
+        raise TypeError("sigma must be a tuple of length two. Got {}"
+                        .format(sigma))
+    ksize_x, ksize_y = ksize
+    sigma_x, sigma_y = sigma
+    kernel_x: torch.Tensor = get_gaussian_kernel(ksize_x, sigma_x)
+    kernel_y: torch.Tensor = get_gaussian_kernel(ksize_y, sigma_y)
+    kernel_2d: torch.Tensor = torch.matmul(
+        kernel_x.unsqueeze(-1), kernel_y.unsqueeze(-1).t())
+    return kernel_2d
+
+
+if __name__ == '__main__':
+    k = get_gaussian_kernel2d((7,7), (0.4,0.4))
+    print('haha')
