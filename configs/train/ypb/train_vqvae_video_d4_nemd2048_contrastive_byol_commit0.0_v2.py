84e654bd80e67babe75ed838ad3705182282bb12
qianduoduolr <656146095@qq.com>
2022-02-19 16:38:54 +0800

    .

--- configs/train/ypb/train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.0_v2.py
+++ configs/train/ypb/train_vqvae_video_d4_nemd2048_contrastive_byol_commit0.0_v2.py
@@ -1,6 +1,7 @@
 import os
 from random import sample
 import sys
+from tkinter.tix import Tree
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from vcl.utils import *
 
@@ -36,9 +37,9 @@ test_cfg = dict(
     precede_frames=20,
     topk=10,
     temperature=0.07,
-    strides=(1, 2, 1, 1),
+    strides=(1, 1, 1, 1),
     out_indices=(3, ),
-    neighbor_range=24,
+    neighbor_range=48,
     with_first=True,
     with_first_neighbor=True,
     output_dir='eval_results')
@@ -159,6 +160,7 @@ eval_config= dict(
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
+test_mode = True
 
 
 
