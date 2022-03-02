a459127a6b286332b12f9d05d39ac4df6abb590c
qianduoduolr <656146095@qq.com>
2022-03-03 00:52:12 +0800

    .

--- configs/train/ypb/vqvae_mlm_d2_nemd2048_byol_dyt_nl_l2_fc_orivq_withbbox_random_v2_mast_3.py
+++ configs/train/ypb/vqvae_mlm_d2_nemd2048_byol_dyt_nl_l2_fc_orivq_withbbox_random_v2_mast_3.py
@@ -3,28 +3,30 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from vcl.utils import *
 
-exp_name = 'vqvae_mlm_d4_nemd2048_byol_dyt_nl_l2_fc_orivq_withbbox_random_v2_39'
+exp_name = 'vqvae_mlm_d2_nemd2048_byol_dyt_nl_l2_fc_orivq_withbbox_random_v2_mast_3'
 docker_name = 'bit:5000/lirui_torch1.8_cuda11.1_corr'
 
 # model settings
 model = dict(
     type='Vqvae_Tracker_V16',
-    backbone=dict(type='ResNet',depth=18, strides=(1, 2, 1, 1), out_indices=(3, )),
-    vqvae=dict(type='VQCL_v7', backbone=dict(type='ResNet', depth=18, strides=(1, 2, 1, 1), out_indices=(2, )),
+    backbone=dict(type='ResNet',depth=18, strides=(1, 1, 1, 1), out_indices=(3, )),
+    vqvae=dict(type='VQCL_v2', backbone=dict(type='ResNet', depth=18, strides=(1, 1, 1, 1), out_indices=(3, ), dilations=(1, 1, 1, 2)),
                sim_siam_head=dict(type='SimSiamHead', in_channels=128, num_projection_fcs=3, projection_mid_channels=128,
                projection_out_channels=128, num_predictor_fcs=2, predictor_mid_channels=128, predictor_out_channels=128,
-               with_norm=True, spatial_type='avg'),loss=dict(type='CosineSimLoss', negative=False), embed_dim=256,
-               n_embed=4096, commitment_cost=1.0,),
+               with_norm=True, spatial_type='avg'),loss=dict(type='CosineSimLoss', negative=False), embed_dim=128,
+               n_embed=2048, commitment_cost=1.0,),
     ce_loss=dict(type='Ce_Loss',reduction='none'),
-    l1_loss=False,
+    l1_loss=True,
     patch_size=-1,
     fc=False,
     temperature=1.0,
-    mask_radius=6,
+    mask_radius=12,
     scaling_att=True,
     temp_window=True,
+    downsample_rate=4,
+    vq_size=64,
     # pretrained='/gdata/lirui/expdir/VCL/group_vqvae_tracker/vqvae_mlm_d4_nemd2048_byol_dyt_nl_l2_fc_orivq_withbbox_random_v2_2/epoch_3200.pth',
-    pretrained_vq='/gdata/lirui/expdir/VCL/group_vqvae_tracker/train_vqvae_video_d4_nemd4096_contrastive_byol_commit1.0_v2_3/epoch_3200.pth',
+    pretrained_vq='/gdata/lirui/expdir/VCL/group_vqvae_tracker/train_vqvae_video_d2_nemd2048_contrastive_byol_commit1.0_v2_4/epoch_3200.pth',
 )
 
 # model training and testing settings
@@ -34,9 +36,9 @@ test_cfg = dict(
     precede_frames=20,
     topk=10,
     temperature=0.07,
-    strides=(1, 2, 1, 1),
+    strides=(1, 1, 1, 1),
     out_indices=(3, ),
-    neighbor_range=24,
+    neighbor_range=48,
     with_first=True,
     with_first_neighbor=True,
     output_dir='eval_results')
@@ -82,7 +84,7 @@ val_pipeline = [
 # demo_pipeline = None
 data = dict(
     workers_per_gpu=2,
-    train_dataloader=dict(samples_per_gpu=32, drop_last=True),  # 4 gpus
+    train_dataloader=dict(samples_per_gpu=16, drop_last=True),  # 4 gpus
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1, workers_per_gpu=1),
 
