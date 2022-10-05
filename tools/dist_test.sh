cd5f52b80ca5cb913541fc01b7320e43bd81ef34
qianduoduolr <5691182+qianduoduolr@user.noreply.gitee.com>
2022-10-05 16:22:09 +0800

    .

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -8,6 +8,6 @@ PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test_prop_dry_run.py --config $CONFIG  --out-indices 2 --launcher pytorch ${@:3}
 
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_prop_dry_run.py --config $CONFIG  --out-indices 3 --launcher pytorch ${@:3}
+# PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#     $(dirname "$0")/test_prop_dry_run.py --config $CONFIG  --out-indices 3 --launcher pytorch ${@:3}
